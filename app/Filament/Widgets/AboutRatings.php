0961f44227eb22dacaa508c84e2dc6b780b43a9e
Horace Ho <horaceh@gmail.com>
2023-10-19 15:23:25 +0800

    Make a Filament About widget

---
+++ app/Filament/Widgets/AboutRatings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class AboutRatings extends Widget
+{
+    protected static ?int $sort = -5;
+    protected static string $view = 'filament.widgets.about-ratings';
+}
---
+++ resources/views/filament/widgets/about-ratings.blade.php
@@ -0,0 +1,31 @@
+<x-filament-widgets::widget class="fi-filament-info-widget">
+    <x-filament::section>
+        <div class="flex items-center gap-x-3">
+            <div class="flex-1">
+                <a
+                    href="/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    <h2 class="text-xl font-semibold text-gray-900 dark:text-white">Ratings</h2>
+                </a>
+
+                <p class="text-xs text-gray-500 dark:text-gray-400">
+                    v1.0.0
+                </p>
+            </div>
+
+            <div class="flex flex-col items-end gap-y-1">
+                <x-filament::link
+                    color="gray"
+                    href="/"
+                    icon="heroicon-o-home"
+                    icon-alias="panels::widgets.about-ratings.home"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                </x-filament::link>
+            </div>
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
